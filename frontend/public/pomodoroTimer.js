ae75eeb8ab7c910c4a9a62441852538bd2d47c57
ctrl-raj <ramkrishnaraj.dev@gmail.com>
2025-10-28 10:11:15 +0530

    changed styles of chatbotScheduler, pomodoro. changed logic of timer

--- frontend/public/pomodoroTimer.js
+++ frontend/public/pomodoroTimer.js
@@ -1,28 +1,30 @@
 const timerDisplay = document.getElementById("timer-display");
 const startBtn = document.getElementById("start-btn");
-const resetBtn = document.getElementById("reset-btn")
-const sessionTitle = document.getElementById("session-title")
+const resetBtn = document.getElementById("reset-btn");
+const sessionTitle = document.getElementById("session-title");
 
 let timer = null;
-let startTime = 0;
-let elapsedTime = 0;
+let startTime = Date.now();
+let totalDuration = 1500000;
 let isRunning = false;
 
 function start(){
     if(!isRunning){
+        elapsedTime = Date.now() - startTime;
         startTime = Date.now() - elapsedTime;
         timer = setInterval(update, 10);
-        isRunning = true
+        isRunning = true;
     }
 }
 function stop(){
     if(isRunning){
         clearInterval(timer);
         elapsedTime = Date.now() - startTime;
+        let remainingTime = totalDuration - elapsedTime;
         isRunning = false;
 
-        let minutes = Math.floor(elapsedTime/(1000 * 60) % 60);
-        let seconds = Math.floor(elapsedTime/1000 % 60);
+        let minutes = Math.floor(remainingTime/(1000 * 60) % 60);
+        let seconds = Math.floor(remainingTime/1000 % 60);
 
         minutes = String(minutes).padStart(2, "0");
         seconds = String(seconds).padStart(2, "0");
@@ -32,25 +34,29 @@ function stop(){
 }
 function reset(){
     clearInterval(timer);
-    startTime = 0;
-    elapsedTime = 0;
+    startTime = Date.now();
+    elapsedTime = Date.now() - startTime;
     isRunning = false;
+    startBtn.textContent = "Start"
 
-    timerDisplay.textContent = `00:00`
+    timerDisplay.textContent = `25:00`;
 }
 
 function update(){
     const currentTime = Date.now();
+    let elapsedTime = currentTime - startTime;
     elapsedTime = currentTime - startTime;
 
-    let minutes = Math.floor(elapsedTime/(1000 * 60) % 60);
-    let seconds = Math.floor(elapsedTime/1000 % 60);
+    let remainingTime = totalDuration - elapsedTime;
+
+    let minutes = Math.floor(remainingTime/(1000 * 60) % 60);
+    let seconds = Math.floor(remainingTime/1000 % 60);
 
     minutes = String(minutes).padStart(2, "0");
     seconds = String(seconds).padStart(2, "0");
     if(minutes=="25"){
         stop();
-        sessionTitle.textContent = "You've Finished 25 Mins of Productivity 🚀"
+        sessionTitle.textContent = "You've Finished 25 Mins of Productivity 🚀";
     }
     else{
         timerDisplay.textContent = `${minutes}:${seconds}`;
